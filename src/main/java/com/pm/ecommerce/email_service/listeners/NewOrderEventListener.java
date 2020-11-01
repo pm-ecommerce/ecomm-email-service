2d82abfaa9f89ba6d95c53e70d6c2c221d94f5f8
Akjol <Akjol.igm@gmail.com>
2020-10-31 21:28:47 -0500

    File Read

--- src/main/java/com/pm/ecommerce/email_service/listeners/NewOrderEventListener.java
+++ src/main/java/com/pm/ecommerce/email_service/listeners/NewOrderEventListener.java
@@ -3,10 +3,16 @@ package com.pm.ecommerce.email_service.listeners;
 import com.pm.ecommerce.email_service.events.NewOrderEvent;
 import com.pm.ecommerce.email_service.repositories.NotificationRepository;
 import com.pm.ecommerce.entities.Notification;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 @Component
 public class NewOrderEventListener implements ApplicationListener<NewOrderEvent> {
@@ -23,11 +29,24 @@ public class NewOrderEventListener implements ApplicationListener<NewOrderEvent>
         notification.setSender("sa.giri@miu.edu");
         notification.setReceiver(orderEvent.getOrder().getUser().getEmail());
         notification.setSubject("We have received your order");
-        String contentsOfFile = "";
+        String html = "";
+        try {
+            File file = new File("../../main/resources/templates/order-received.html");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                html += data;
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        Document doc = Jsoup.parse(html);
         // read the file contents
         // populate the file with real data
         // set the populated file as notification message
-        notification.setMessage(contentsOfFile);
+        notification.setMessage(doc.body().toString());
         repository.save(notification);
         template.send("NotificationTopic", notification.getId());
     }
